2f3130acb18832fde1002ddde6fcf7886def8e57
felippe weber <felippetaw@gmail.com>
2018-09-11 12:40:11 -0300

    aula 1 de refatoração

---
+++ REFATORAÇÃO PARTE 1/RefatoracaoParte1/src/pkgRefatoracao/Refatoracao.java
@@ -0,0 +1,40 @@
+package pkgRefatoracao;
+import java.util.*;
+public class Refatoracao {
+	private static final int QUANTIDADE = 3;
+	static Calendar calendario;
+	static Scanner entrada;
+	public static void main(String[] args) {
+		double mensalidade=0;
+		double novaMensalidade=0;
+		int mes=0;
+		int mesAtual=0;
+		entrada = new Scanner(System.in);
+		int contador=1;
+		while(contador <=QUANTIDADE){
+			System.out.println("\nValor da mensalidade: ");
+			mensalidade = entrada.nextDouble();
+			System.out.println("Mês de referência: ");
+			mes = entrada.nextInt();
+			calendario = Calendar.getInstance();
+			mesAtual = calendario.get(Calendar.MONTH) +1;
+			novaMensalidade = verificarValorMensalidade(mensalidade, novaMensalidade, mes, mesAtual);
+			imprimirResultados(mensalidade, novaMensalidade, mes, mesAtual);
+		}	
+	}
+	private static void imprimirResultados(double mensalidade, double novaMensalidade, int mes, int mesAtual) {
+		System.out.println("RESULTADO");
+		System.out.println("Mês atual: "+mesAtual);
+		System.out.println("Referência "+mes);
+		System.out.println("Mensalidade: "+mensalidade);
+		System.out.println("Novo valor: "+novaMensalidade);
+	}
+	private static double verificarValorMensalidade(double mensalidade, double novaMensalidade, int mes, int mesAtual) {
+		if(mes<mesAtual) {
+			double valorCalculadoMensalidade = novaMensalidade = mensalidade *1.10;
+		}else if(mes == mesAtual) {
+			novaMensalidade = mensalidade;
+		}
+		return novaMensalidade;
+	}
+}
